64cde24ad4e02a11ce953ec9f858afb4c94f36b5
Bindulatha kondapalli <120655881+Bindulatha9@users.noreply.github.com>
2023-06-08 17:18:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ access_key = ""
 secret_key = "s"
 }
 resource "aws_instance" "key" {
-ami="ami-04669a22aad391419"
+ami="ami"
 instance_type= "t2.micro"
 key_name= "sshkey"
 vpc_security_group_ids=[aws_security_group.demo_sg.id]
